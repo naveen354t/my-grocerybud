4d7185d2c5ec140fc009b2ecebb67df89aa4e47b
naveen354t <naveen354t@gmail.com>
2022-11-01 19:03:41 +0530

    firstcommit

---
+++ src/Alert.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeOut);
+  }, [list,removeAlert]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+export default Alert;
